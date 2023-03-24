be60d976b88559b9259798f8e335f8582aa13f1a
[Edwin Velasco] <evelasco1423@gmail.com>
2023-03-24 15:26:37 -0400

    Initial commit

---
+++ src/test/java/Assestment.java
@@ -0,0 +1,68 @@
+public class Assestment {
+    /* Question 1
+    public static void main(String[] args) {
+        System.out.println("Edwin Velasco");
+
+*/
+    /* Question 2
+    static void greetUser (String username){
+        System.out.println("Welcome to my website " + username);
+    }
+
+    public static void main(String[] args) {
+        greetUser("EdwinV");
+    }
+*/
+/*Question 3
+    static int buyCoffee(int x) {
+        return 1 - x;
+    }
+    public static void main(String[] args) {
+        System.out.println(buyCoffee(3));
+}
+
+*/
+/*  Question 4 ---- Missing stuff
+    static String [] myHobbies(String [] hobbies) {
+        hobbies = ["reading", "music", "driving"]
+        return hobbies;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hobbies);
+    }
+*/
+    /* question 5
+    public static String checkWeather(int temperature) {
+        if (temperature >= 80) {
+            return "Wear shorts";
+        } else if (temperature >= 50) {
+            return "Wear wind breakers";
+        } else if (temperature <= 50) {
+            return "Wear a jacket";
+        } else {
+            return "No such weather condition";
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(checkWeather(80));
+    }
+*/
+    /*  question 6
+    public static double areaOfTriangle(double base, double height) {
+        return 0.5 * base * height;
+    }
+    public static void main(String[] args) {
+        System.out.println(areaOfTriangle(2,3));
+    }
+*/
+    // Question 7
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int rand_int1 = rand.nextInt(1000);
+        int randoTwo = 6;
+        System.out.println();
+    }
+}
+
+}
